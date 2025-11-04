abd6fb8b3f0b10d42565a8f9377e0d2df3808ddb
Fabricio Cervantes <fabriciocervantesmendoza@gmail.com>
2025-11-04 09:53:02 -0500

    feat: Mover codigo de appcomponet a main-layout para solo vistas de negocio, separando de notfound.component

--- front-task-gestion/src/app/app.routes.ts
+++ front-task-gestion/src/app/app.routes.ts
@@ -1,12 +1,30 @@
 import { Routes } from '@angular/router';
+import { MainLayoutComponent } from './share/main-layout/main-layout.component';
+import { NotFoundComponent } from './share/not-found/not-found.component';
 
 export const routes: Routes = [
     {
-        path: 'task',
-        loadChildren: () => import('./task/task.routes').then(m => m.routes)
+        path:'',
+        component: MainLayoutComponent,
+        children: [
+            {
+                path: '',
+                redirectTo: 'task/historial',
+                pathMatch: 'full'
+            },
+            {
+                path: 'task',
+                loadChildren: () => import('./task/task.routes').then(m => m.routes)
+            }
+        ]
     },
     {
-        path:'**',
-        redirectTo: 'task/gestion'
+        path: '404',
+        component: NotFoundComponent
+    },
+    // Wildcard al final
+    {
+    path: '**',
+    redirectTo: '404'
     }
 ];
--- front-task-gestion/src/app/share/main-layout/main-layout.component.ts
+++ front-task-gestion/src/app/share/main-layout/main-layout.component.ts
@@ -1,9 +1,11 @@
 import { Component } from '@angular/core';
+import { SidebarComponent } from "../sidebar/sidebar.component";
+import { RouterModule } from "@angular/router";
 
 @Component({
   selector: 'app-main-layout',
   standalone: true,
-  imports: [],
+  imports: [SidebarComponent, RouterModule],
   templateUrl: './main-layout.component.html',
   styleUrl: './main-layout.component.css'
 })
