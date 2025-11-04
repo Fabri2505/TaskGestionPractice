52ae758a43f55d1f76187b7d18994faac455352f
Fabricio Cervantes <fabriciocervantesmendoza@gmail.com>
2025-11-04 10:33:20 -0500

    feat: Add diseño para componente de notfounf

--- front-task-gestion/src/app/share/not-found/not-found.component.ts
+++ front-task-gestion/src/app/share/not-found/not-found.component.ts
@@ -1,12 +1,36 @@
-import { Component } from '@angular/core';
+import { animate, state, style, transition, trigger } from '@angular/animations';
+import { Component, inject } from '@angular/core';
+import { RouterModule } from '@angular/router';
+import { ButtonModule } from 'primeng/button';
+import { RippleModule } from 'primeng/ripple';
+import { Location } from '@angular/common';
 
 @Component({
   selector: 'app-not-found',
   standalone: true,
-  imports: [],
+  imports: [RouterModule, ButtonModule, RippleModule],
   templateUrl: './not-found.component.html',
-  styleUrl: './not-found.component.css'
+  styleUrl: './not-found.component.css',
+  animations: [
+    trigger('fadeInUp', [
+      state('void', style({
+        opacity: 0,
+        transform: 'translateY(30px)'
+      })),
+      transition(':enter', [
+        animate('600ms ease-out', style({
+          opacity: 1,
+          transform: 'translateY(0)'
+        }))
+      ])
+    ])
+  ]
 })
 export class NotFoundComponent {
+  private location = inject(Location);
+
+  goBack() {
+    this.location.back();
+  }
 
 }
