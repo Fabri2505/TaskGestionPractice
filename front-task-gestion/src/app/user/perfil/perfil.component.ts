38901e2ec1ea898bada4e33344784dbfdd78a2d1
Fabricio Cervantes <fabriciocervantesmendoza@gmail.com>
2025-11-04 21:10:45 -0500

    feat: integrar preferencia en perfil componente

--- front-task-gestion/src/app/user/perfil/perfil.component.ts
+++ front-task-gestion/src/app/user/perfil/perfil.component.ts
@@ -1,36 +1,24 @@
-import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
-import { FormsModule } from '@angular/forms';
 import { CardModule } from 'primeng/card';
 import { ButtonModule } from 'primeng/button';
-import { InputTextModule } from 'primeng/inputtext';
-import { TextareaModule  } from 'primeng/textarea';
-import { PasswordModule } from 'primeng/password';
 import { AvatarModule } from 'primeng/avatar';
-import { MessageModule } from 'primeng/message';
-import { DividerModule } from 'primeng/divider';
 import { InputSwitchModule } from 'primeng/inputswitch';
 import { InfoPersonalComponent } from '../ui/info-personal/info-personal.component';
 import { CambioContraComponent } from "../ui/cambio-contra/cambio-contra.component";
+import { PreferenciaComponent } from '../ui/preferencia/preferencia.component';
 
 @Component({
   selector: 'app-perfil',
   standalone: true,
   imports: [
-    InfoPersonalComponent,
-    CommonModule,
-    FormsModule,
     CardModule,
     ButtonModule,
-    InputTextModule,
-    TextareaModule,
-    PasswordModule,
     AvatarModule,
-    MessageModule,
-    DividerModule,
     InputSwitchModule,
-    CambioContraComponent
-],
+    InfoPersonalComponent,
+    CambioContraComponent,
+    PreferenciaComponent
+  ],
   templateUrl: './perfil.component.html',
   styleUrl: './perfil.component.css'
 })
--- front-task-gestion/src/app/user/ui/preferencia/preferencia.component.ts
+++ front-task-gestion/src/app/user/ui/preferencia/preferencia.component.ts
@@ -1,9 +1,18 @@
 import { Component } from '@angular/core';
+import { CardModule } from 'primeng/card';
+import { DividerModule } from 'primeng/divider';
+import { InputSwitchModule } from 'primeng/inputswitch';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-preferencia',
   standalone: true,
-  imports: [],
+  imports: [
+        FormsModule,
+        CardModule,
+        DividerModule,
+        InputSwitchModule
+  ],
   templateUrl: './preferencia.component.html',
   styleUrl: './preferencia.component.css'
 })
