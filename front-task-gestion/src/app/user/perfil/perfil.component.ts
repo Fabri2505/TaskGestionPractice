5b5b030216637add59a37fad5ed2d5b25d4ec3f5
Fabricio Cervantes <fabriciocervantesmendoza@gmail.com>
2025-11-06 10:38:58 -0500

    Conexion con BACKEND para cambiar contraseña. Implementado en componente de cambio-contra e injeccion en perfilComponent

--- front-task-gestion/src/app/user/perfil/perfil.component.ts
+++ front-task-gestion/src/app/user/perfil/perfil.component.ts
@@ -1,9 +1,13 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
 import { InputSwitchModule } from 'primeng/inputswitch';
 import { InfoPersonalComponent } from '../ui/info-personal/info-personal.component';
 import { CambioContraComponent } from "../ui/cambio-contra/cambio-contra.component";
 import { PreferenciaComponent } from '../ui/preferencia/preferencia.component';
 import { AvatarComponent } from '../ui/avatar/avatar.component';
+import { ChangePasswordRequest } from '../utils/schemas';
+import { PerfilService } from '../data-access/perfil.service';
+import { MessageService } from 'primeng/api';
+import { Toast } from 'primeng/toast';
 
 @Component({
   selector: 'app-perfil',
@@ -11,13 +15,34 @@ import { AvatarComponent } from '../ui/avatar/avatar.component';
   imports: [
     AvatarComponent,
     InputSwitchModule,
+    Toast,
     InfoPersonalComponent,
     CambioContraComponent,
     PreferenciaComponent
   ],
+  providers: [MessageService],
   templateUrl: './perfil.component.html',
   styleUrl: './perfil.component.css'
 })
 export class PerfilComponent {
 
+  private perfilServ = inject(PerfilService);
+  private messageService = inject(MessageService);
+
+  passwordChanged(passwordObj: ChangePasswordRequest) {
+    // Aquí puedes manejar el evento de cambio de contraseña
+
+    this.perfilServ.changePassword(passwordObj).subscribe({
+      next: (response) => {
+        console.log('Respuesta del servidor:', response);
+        this.messageService.add({severity:'success', summary:'Éxito', detail:response.message});
+        // Aquí puedes agregar lógica adicional, como mostrar un mensaje de éxito
+      },
+      error: (error) => {
+        console.error('Error al cambiar la contraseña:', error);
+        this.messageService.add({severity:'error', summary:'Error', detail:'No se pudo cambiar la contraseña. Por favor, inténtelo de nuevo.'});
+        // Aquí puedes manejar el error, como mostrar un mensaje de error al usuario
+      }
+    });
+  }
 }
--- front-task-gestion/src/app/user/ui/cambio-contra/cambio-contra.component.ts
+++ front-task-gestion/src/app/user/ui/cambio-contra/cambio-contra.component.ts
@@ -1,21 +1,93 @@
-import { Component } from '@angular/core';
+import { Component, EventEmitter, Output } from '@angular/core';
 import { ButtonModule } from 'primeng/button';
 import { CardModule } from 'primeng/card';
 import { MessageModule } from 'primeng/message';
 import { PasswordModule } from 'primeng/password';
+import { FormsModule } from "@angular/forms";
+import { ChangePasswordRequest } from '../../utils/schemas';
 
 @Component({
   selector: 'app-cambio-contra',
   standalone: true,
   imports: [
-    ButtonModule, 
-    CardModule, 
+    ButtonModule,
+    CardModule,
     PasswordModule,
-    MessageModule
-  ],
+    MessageModule,
+    FormsModule
+],
   templateUrl: './cambio-contra.component.html',
   styleUrl: './cambio-contra.component.css'
 })
 export class CambioContraComponent {
 
+  oldContra:string = '';
+  newContra:string = '';
+  confirmContra:string = '';
+
+  errorMessage: string = '';
+  showError: boolean = false;
+
+  @Output() passwordChanged = new EventEmitter<ChangePasswordRequest>();
+
+  sendPasswordChange() {
+    this.showError = false;
+    this.errorMessage = '';
+
+    if (!this.oldContra ||!this.newContra || !this.confirmContra){
+      this.mostrarError('Por favor, complete todos los campos.');
+      return;
+    }
+
+    if (this.newContra !== this.confirmContra) {
+      this.mostrarError('La nueva contraseña y la confirmación no coinciden.');
+      return;
+    }
+
+    if (this.newContra.length < 8) {
+      this.mostrarError('La contraseña debe tener al menos 8 caracteres');
+      return;
+    }
+
+    // Validación 4: Complejidad (mayúsculas, minúsculas, números)
+    const tieneMinuscula = /[a-z]/.test(this.newContra);
+    const tieneMayuscula = /[A-Z]/.test(this.newContra);
+    const tieneNumero = /[0-9]/.test(this.newContra);
+    
+    if (!tieneMinuscula || !tieneMayuscula || !tieneNumero) {
+      this.mostrarError('La contraseña debe incluir mayúsculas, minúsculas y números');
+      return;
+    }
+    
+    // Validación 5: La nueva no puede ser igual a la actual
+    if (this.oldContra === this.newContra) {
+      this.mostrarError('La nueva contraseña debe ser diferente a la actual');
+      return;
+    }
+
+    const changePasswordRequest: ChangePasswordRequest = {
+      userId: 1,
+      old_password: this.oldContra,
+      new_password: this.newContra
+    };
+
+    this.passwordChanged.emit(changePasswordRequest);
+  }
+
+  limpiarCampos() {
+    this.oldContra = '';
+    this.newContra = '';
+    this.confirmContra = '';
+  }
+
+  mostrarError(message: string) {
+    this.errorMessage = message;
+    this.showError = true;
+  }
+
+  cancelar(){
+    this.limpiarCampos();
+    this.showError = false;
+    this.errorMessage = '';
+  }
 }
